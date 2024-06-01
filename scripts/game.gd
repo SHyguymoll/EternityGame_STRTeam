761e79dcd0bec8cd481f83058bd4ba6121110233
SHyguymoll <shymollet@gmail.com>
2024-05-31 17:40:13 -0700

    reparented ground placeholder for proper looping

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,4 +3,4 @@ extends Node3D
 @onready var _ground : Node3D = $GroundPlaceholder
 
 func _process(delta: float) -> void:
-	_ground.position.z = fmod(_ground.position.z + (delta * 100), 55)
+	_ground.position.z = fmod(_ground.position.z + (delta * 100), 100)
