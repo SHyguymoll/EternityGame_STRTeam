cbf3168befbb46413bcd311aa5e23e02afe7c975
SHyguymoll <shymollet@gmail.com>
2024-06-15 19:28:05 -0700

    holder for enemies & a destroyer for freeing them

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,4 +4,6 @@ extends Node3D
 
 func _process(delta: float) -> void:
 	_ground.position.x = fmod(_ground.position.x - (delta * 100), 100)
+func _on_enemy_destroyer_area_entered(area: Area3D) -> void:
+	area.queue_free()
 	pass
