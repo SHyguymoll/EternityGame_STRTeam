6746cc2aca2be1cecf865046fe870032c537d710
anvilsp <avastquest@gmail.com>
2024-06-16 21:18:04 -0700

    preliminary acceleration-based movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,11 @@ class_name Player
 extends CharacterBody3D
 
 
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const ACCELERATION = 8
+const MAX_SPEED = 20
+
+var motion : Vector2 = Vector2.ZERO
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
--- scripts/state_machine/player_state/player_base_state.gd
+++ scripts/state_machine/player_state/player_base_state.gd
@@ -1,5 +1,7 @@
 extends State
 
+@export var allow_horizontal_input := true
+
 # base state script: other states should inherit from this one
 
 func _enter_state():
@@ -18,10 +20,12 @@ func tick_movement(delta): # wrapper for functions that can be overwritten in ot
 func apply_movement(delta): # handle horizontal movement
 	var input_dir = Input.get_axis("player_left", "player_right")
 	var direction = (host.transform.basis * Vector3(input_dir, 0, 0)).normalized()
-	if direction:
-		host.velocity.x = direction.x * host.SPEED
+	host.motion.x = get_acceleration() * input_dir * delta
+	if direction and allow_horizontal_input:
+		host.velocity.x += host.motion.x
+		host.velocity.x = clamp(host.velocity.x, -get_max_speed(), get_max_speed())
 	else:
-		host.velocity.x = move_toward(host.velocity.x, 0, host.SPEED)
+		host.velocity.x = move_toward(host.velocity.x, 0, get_acceleration() * delta)
 
 func jump(delta):
 	if Input.is_action_just_pressed("player_jump") and host.is_on_floor():
@@ -46,3 +50,9 @@ func apply_gravity(delta):
 
 func get_gravity():
 	return host.gravity
+
+func get_acceleration():
+	return host.ACCELERATION
+
+func get_max_speed():
+	return host.MAX_SPEED
