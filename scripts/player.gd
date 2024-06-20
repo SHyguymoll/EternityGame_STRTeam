3de3becacdd66b1c9f48dee7eecbdeca6eb583f1
anvilsp <avastquest@gmail.com>
2024-06-19 18:53:44 -0700

    value changes + buffer jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,19 @@
 class_name Player
 extends CharacterBody3D
 
-const JUMP_VELOCITY : int = 10
+const JUMP_VELOCITY : int = 15
 const ACCELERATION : int = 15
 const MAX_SPEED : int = 10
 const DODGE_DISTANCE : float = 0.5
 const DODGE_TIME : float = 0.5
+const BUFFER_FRAME : float = 0.1
 
 @onready var state_machine = $StateMachine
 @onready var animation_player = $AnimationPlayer
 @onready var default_z_position : float = position.z
 
 var motion : Vector2 = Vector2.ZERO
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 3 # i could change this in the project settings but i'm anticipating a merge conflict LMAO
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 6 # i could change this in the project settings but i'm anticipating a merge conflict LMAO
 
 func _ready():
 	state_machine.set_state("idle_run")
--- scripts/state_machine/player_state/jump_state.gd
+++ scripts/state_machine/player_state/jump_state.gd
@@ -1,12 +1,33 @@
 extends "res://scripts/state_machine/player_state/player_base_state.gd"
 
+var buffer_timer : float = 0.0
+var buffered : bool = false
+
+func _enter_state():
+	reset_buffers()
+
 func _tick_state(delta):
 	tick_movement(delta)
+	if buffered:
+		buffer_timer += delta
+	if Input.is_action_just_pressed("player_jump") and not buffered and not host.is_on_floor(): # enable buffer
+		buffered = true
 	if not Input.is_action_pressed("player_jump") and host.velocity.y > (host.JUMP_VELOCITY/1.5):
 		host.velocity.y = host.JUMP_VELOCITY / 1.5
-		host.state_machine.set_state("idle_run")
 	if host.is_on_floor() and host.velocity.y == 0:
-		host.state_machine.set_state("idle_run")
+		if buffered and buffer_timer < host.BUFFER_FRAME:
+			reset_buffers()
+			host.velocity.y += host.JUMP_VELOCITY
+			host.state_machine.set_state("jump")
+		else:
+			host.state_machine.set_state("idle_run")
+
+func _exit_state():
+	reset_buffers()
+
+func reset_buffers():
+	buffer_timer = 0.0
+	buffered = false
 
 func get_gravity():
 	return host.gravity
